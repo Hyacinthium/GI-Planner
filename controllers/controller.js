0f329097db586d040735ea2b2843219ece8e889e
Cyril Ethan de Guzman <cyril_deguzman@dlsu.edu.ph>
2021-06-04 03:13:52 +0800

    added controller functions

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,8 +2,6 @@ const db = require('../models/db.js');
 
 const controller = {
 
-    
-
     /**
      * getIndex.
      * 
@@ -13,10 +11,16 @@ const controller = {
      */
     getIndex: function(req, res) {
         res.render('index', {name:"Leana"});   //hbs
-        
-    }   // >,< if meron pa sunod
+    },   // >,< if meron pa sunod
 
-    
+    getSignUp: function(req, res) {
+        // add to routes and render sign up
+    },
+
+    getSignIn: function(req, res) {
+        // add to routes and render login
+    },
+
 }
 
 module.exports = controller;
