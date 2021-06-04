a27807039208e9c33be7693c6b3408ca7fdd4333
Hyacinthium <leana_rebong@dlsu.edu.ph>
2021-06-04 08:32:32 +0800

    update profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,9 +2,58 @@ $(document).ready(function () {
 
     //id
     $('#chara-submit').click(function(){
-        let about = $('#chara-input');
+        let character = $('#chara-input');
         
-        $.get('/updateabout', {about: about.val()}, function(data, status){});
+        $.get('/updateCharacter', {character: character.val()}, function(data, status){});
+        window.location = '/profile';
+    });
+
+    $('#vision-submit').click(function(){
+        let vision = $('#vision-input');
+        
+        $.get('/updateVision', {vision: vision.val()}, function(data, status){});
+        window.location = '/profile';
+    });
+
+    $('#weap-submit').click(function(){
+        let weapon = $('#weap-input');
+        
+        $.get('/updateWeapon', {weapon: weapon.val()}, function(data, status){});
+        window.location = '/profile';
+    });
+
+    $('#flower-submit').click(function(){
+        let flower = $('#flower-input');
+        
+        $.get('/updateFlower', {flower: flower.val()}, function(data, status){});
+        window.location = '/profile';
+    });
+
+    $('#plume-submit').click(function(){
+        let plume = $('#plume-input');
+        
+        $.get('/updatePlume', {plume: plume.val()}, function(data, status){});
+        window.location = '/profile';
+    });
+
+    $('#sands-submit').click(function(){
+        let sands = $('#sands-input');
+        
+        $.get('/updateSands', {sands: sands.val()}, function(data, status){});
+        window.location = '/profile';
+    });
+
+    $('#goblet-submit').click(function(){
+        let goblet = $('#goblet-input');
+        
+        $.get('/updateGoblet', {goblet: goblet.val()}, function(data, status){});
+        window.location = '/profile';
+    });
+
+    $('#circlet-submit').click(function(){
+        let circlet = $('#circlet-input');
+        
+        $.get('/updateCirclet', {circlet: circlet.val()}, function(data, status){});
         window.location = '/profile';
     });
 })
